b03617b6301197bf03ad08ef4ba8db41da2b4bbb
sopiro <sopiro.ldh@gmail.com>
2025-09-29 05:04:43 +0900

    Add transform builder functions Update quaternion front facing rule

--- include/wak/transform.h
+++ include/wak/transform.h
@@ -102,6 +102,32 @@ struct Transform
     {
         return Transform{ q.RotateInv(-p), q.GetConjugate(), 1 / s };
     }
+
+    WAK_CPU_GPU
+    static Transform Translate(const Vec3& position)
+    {
+        return Transform(position, Quat(1), Vec3(1));
+    }
+
+    WAK_CPU_GPU
+    static Transform Rotate(const Vec3& rotation)
+    {
+        return Transform(Vec3(0), Quat::FromEuler(rotation), Vec3(1));
+    }
+
+    WAK_CPU_GPU
+    static Transform Scale(const Vec3& scale)
+    {
+        return Transform(Vec3(0), Quat(1), scale);
+    }
+
+    WAK_CPU_GPU
+    static Transform LookAt(const Vec3& position, const Vec3& target, const Vec3& up)
+    {
+        Vec3 w = target - position;
+        w.Normalize();
+        return Transform(position, Quat(w, up));
+    }
 };
 
 WAK_CPU_GPU constexpr inline bool operator==(const Transform& a, const Transform& b)
